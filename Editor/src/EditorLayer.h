7684704022e2e5b1e0920e99067c0f8af01ac928
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-07-21 08:57:13 -0700

    Added Editor Project

---
+++ Editor/src/EditorLayer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Ongine.h>
+class EditorLayer : public ON::Layer {
+public:
+	virtual void OnRun() override {
+	}
+	virtual void OnUpdate(ON::Input& input) override {
+	}
+	virtual void OnRender() override {
+	}
+};
