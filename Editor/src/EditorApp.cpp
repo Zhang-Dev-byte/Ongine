bf5b38b7578c7a9e8dfad1355823d58ac2497c44
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-07-21 10:59:01 -0700

    Added Properties Panel and Code Cleanup

--- Editor/src/EditorApp.cpp
+++ Editor/src/EditorApp.cpp
@@ -9,6 +9,7 @@ public:
 	ON::Entity e = ON::Entity({"Entity"}, s.GetRegistry());
 	ON::Entity c = ON::Entity({"Camera"}, s.GetRegistry());
 	ON::OpenGLFramebuffer FBO;
+	entt::entity sentity = s.GetRegistry().create();
 
 	virtual void OnRun() override {
 		FBO.Create({ 1280, 720 });
@@ -27,70 +28,51 @@ public:
 		s.Render();
 		FBO.Unbind();
 
-		static bool dockspaceOpen = true;
-		static bool opt_fullscreen_persistant = true;
-		bool opt_fullscreen = opt_fullscreen_persistant;
-		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+		Dockspace::Begin();
 
-		// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-		// because it would be confusing to have two docking targets within each others.
-		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-		if (opt_fullscreen)
-		{
-			ImGuiViewport* viewport = ImGui::GetMainViewport();
-			ImGui::SetNextWindowPos(viewport->Pos);
-			ImGui::SetNextWindowSize(viewport->Size);
-			ImGui::SetNextWindowViewport(viewport->ID);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-		}
-
-		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-		if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-			window_flags |= ImGuiWindowFlags_NoBackground;
-
-		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
-		// all active windows docked into it will lose their parent and become undocked.
-		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
-		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-		ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
-		ImGui::PopStyleVar();
-
-		if (opt_fullscreen)
-			ImGui::PopStyleVar(2);
-
-		// DockSpace
-		ImGuiIO& io = ImGui::GetIO();
-		if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-		{
-			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-		}
-
-		if (ImGui::BeginMenuBar())
-		{
-			if (ImGui::BeginMenu("File"))
-			{
-				ImGui::EndMenu();
-			}
-
-			ImGui::EndMenuBar();
-		}
 		ImGui::Begin("Viewport");
 		ImGui::Image((void*)FBO.GetTCB(), ImVec2(1280 / 1.5f, 720 / 1.5f));
 		ImGui::End();
 
 		ImGui::Begin("Control");
-		ImGui::DragFloat2("Position", glm::value_ptr(e.GetComponent<ON::Transform>(s.GetRegistry()).GetPosition()), 0.01f);
+		auto view = s.GetRegistry().view<ON::EntityInfo>();
+		for (auto e : view) {
+			if (ImGui::Button(s.GetRegistry().get<ON::EntityInfo>(e).name.c_str(), ImVec2(100, 20))) {
+				sentity = e;
+			}
+		}
 		ImGui::End();
 
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
-		ImGui::PopStyleVar();
+		ImGui::Begin("Properties");
+		if (s.GetRegistry().has<ON::EntityInfo>(sentity)){
+			ImGui::Text(s.GetRegistry().get<ON::EntityInfo>(sentity).name.c_str());
+			if (s.GetRegistry().has<ON::Transform>(sentity)) {
+				// Entity
+				ImGui::DragFloat2("Position", glm::value_ptr(s.GetRegistry().get<ON::Transform>(sentity).GetPosition()), 0.01f);
+				ImGui::DragFloat2("Scale", glm::value_ptr(s.GetRegistry().get<ON::Transform>(sentity).GetScale()));
+
+				//Rotation Code Sorry this is kindof messy
+				float rot = (s.GetRegistry().get<ON::Transform>(sentity).GetRotation());
+				ImGui::DragFloat("Rotation", &rot);
+				s.GetRegistry().get<ON::Transform>(sentity).SetRotation(rot);
+			}
+			else if(s.GetRegistry().has<ON::Camera>(sentity)){
+				// Camera
+				ImGui::DragFloat2("Position", glm::value_ptr(s.GetRegistry().get<ON::Camera>(sentity).GetPosition()), 0.01f);
+
+				//Rotation Code Sorry this is kindof messy
+				float rot = (s.GetRegistry().get<ON::Camera>(sentity).GetRotation());
+				ImGui::DragFloat("Rotation", &rot);
+				s.GetRegistry().get<ON::Camera>(sentity).SetRotation(rot);
+			}
+			else {
+				// Invalid
+				ON_CORE_ERROR("Invalid Entity!");
+			}
+		}
 		ImGui::End();
+
+		Dockspace::End();
 	}
 };
 
